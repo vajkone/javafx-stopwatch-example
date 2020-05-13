6e52ae6b43750d59631a607d800a34b71cafc496
Vajk Garbóczy <vajkone18@gmail.com>
2020-05-13 18:31:54 +0200

    Added Button for pausing and resuming timer

--- src/main/java/stopwatch/Main.java
+++ src/main/java/stopwatch/Main.java
@@ -2,7 +2,8 @@ package stopwatch;
 
 import javafx.application.Application;
 
-public class Main {
+
+public class Main{
 
     public static void main(String[] args) {
         Application.launch(StopWatchApplication.class, args);
--- src/main/java/stopwatch/StopWatchApplication.java
+++ src/main/java/stopwatch/StopWatchApplication.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -15,31 +16,67 @@ import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import java.io.IOException;
 
-public class StopWatchApplication extends Application {
+public class StopWatchApplication extends Application  {
 
     @FXML
     private Label stopWatchLabel;
+    @FXML
+    private Button timerButton;
 
     private long start;
+    private Timeline clock;
+    private long timeelapsed;
+    private long pausedtime = 0;
+    private long paused=0;
+
+
+
 
     @FXML
     public void initialize() {
-        start = System.currentTimeMillis();
-        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
-            long millisElapsed = System.currentTimeMillis() - start;
-            stopWatchLabel.setText(DurationFormatUtils.formatDuration(millisElapsed, "HH:mm:ss"));
+
+
+        timerButton.setText("Pause");
+
+        start= System.currentTimeMillis();
+        clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+
+            timeelapsed = System.currentTimeMillis() - start-pausedtime;
+
+            stopWatchLabel.setText(DurationFormatUtils.formatDuration(timeelapsed, "HH:mm:ss"));
         }), new KeyFrame(Duration.seconds(1)));
         clock.setCycleCount(Animation.INDEFINITE);
+
         clock.play();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(StopWatchApplication.class.getResource("/stopwatch.fxml"));
+
+
         stage.setTitle("JavaFX Timer");
         stage.setResizable(false);
         stage.setScene(new Scene(root));
         stage.show();
     }
+
+    public void startButtonClicked(){
+
+        if (timerButton.getText().equals("Pause")){
+            clock.pause();
+            paused=System.currentTimeMillis();
+            timerButton.setText("Resume");
+
+        }else{
+            timerButton.setText("Pause");
+            long starttime = System.currentTimeMillis();
+            pausedtime+=starttime-paused;
+
+            clock.play();
+       }
+
+    }
+
 
 }
